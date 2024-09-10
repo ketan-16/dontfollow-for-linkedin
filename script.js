fa78963aaa826a7535d194620bcf859866f4df46
Ketan Y <ktnydv@gmail.com>
2024-09-11 02:59:57 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         DontFollow for Linkedin
+// @namespace    http://tampermonkey.net/
+// @version      0.1a
+// @description  Unticks the annoying "Follow company for more updates" in the final Easy Apply modal.
+// @author       Ketan Yadav
+// @match        https://www.linkedin.com/jobs/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=linkedin.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    console.log("[USERSCRIPT]: DontFollow for LinkedIn Started!")
+    const uncheckFollowCompany = () => {
+        // Get all elements with the same ID
+        const elements = document.querySelectorAll('#follow-company-checkbox');
+        elements.forEach((element) => {
+            // Check if the element is an input checkbox
+            if (element.tagName === 'INPUT' && element.type === 'checkbox') {
+                if (element.checked) {
+                    element.checked = false; // Uncheck it
+                    console.log("[USERSCRIPT]: Element Unchecked!")
+                }
+            }
+        });
+    };
+
+    // Check every second for the checkbox
+    const interval = setInterval(uncheckFollowCompany, 1000);
+
+})()
